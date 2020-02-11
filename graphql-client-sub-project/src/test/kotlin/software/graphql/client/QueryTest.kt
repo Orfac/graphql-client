7111804a4839ba4b31004fdc9b6225644b095e83
arseniy <molodeckij.b.2017@fml31.ru>
2020-02-12 00:45:28 +0300

    add another test for building query for class with no properties

--- graphql-client-sub-project/src/test/kotlin/software/graphql/client/QueryTest.kt
+++ graphql-client-sub-project/src/test/kotlin/software/graphql/client/QueryTest.kt
@@ -6,14 +6,27 @@ import kotlin.test.assertEquals
 
 public class QueryTest{
 
-    public data class Product(val id : Int, val name : String )
+
     @Test
     public fun `building query works for simple class with 2 properties` (){
+        data class Product(val id : Int, val name : String )
+
         val myProduct = Product(5,"pie")
         val query = Query()
 
         assertEquals(
             "Product {\n\tid\n\tname\n}",
+            query.buildGraphQL(myProduct)
+        )
+    }
+
+    @Test
+    public fun `building query works for class with no properties`(){
+        class Product()
+        val myProduct = Product()
+        val query = Query()
+        assertEquals(
+            "Product {}",
             query.buildGraphQL(myProduct)
         )
     }
