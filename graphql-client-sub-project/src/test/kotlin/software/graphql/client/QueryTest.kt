d5bbbc24b7f2e6231fe4d6d8dcaa3dc91077d2e3
arseniy <molodeckij.b.2017@fml31.ru>
2020-02-12 03:35:02 +0300

    add rendering for classes inside each other

--- graphql-client-sub-project/src/test/kotlin/software/graphql/client/QueryTest.kt
+++ graphql-client-sub-project/src/test/kotlin/software/graphql/client/QueryTest.kt
@@ -26,7 +26,36 @@ public class QueryTest{
         val myProduct = Product()
         val query = Query()
         assertEquals(
-            "Product {}",
+            "Product {\n}",
+            query.buildGraphQL(myProduct)
+        )
+    }
+
+    @Test
+    public fun `building query works for class with a class inside`(){
+        data class SubProduct(val id : Int, val name : String)
+        data class Product(val id : Int, val subProduct: SubProduct )
+
+        val myProduct = Product(5,SubProduct(1,"pie"))
+        val query = Query()
+        println(query.buildGraphQL(myProduct))
+        assertEquals(
+            "Product {\n\tid\n\tSubProduct {\n\tid\n\tname\n}\n}",
+            query.buildGraphQL(myProduct)
+        )
+    }
+
+    @Test
+    public fun `building query works for class 2 level classes inside each other`(){
+        data class SubProduct2(val id : Int, val name : String)
+        data class SubProduct1(val id : Int, val subProduct2: SubProduct2)
+        data class Product(val id : Int, val subProduct: SubProduct1 )
+
+        val myProduct = Product(5,SubProduct1(1,SubProduct2(1,"pie")))
+        val query = Query()
+        println(query.buildGraphQL(myProduct))
+        assertEquals(
+            "Product {\n\tid\n\tSubProduct1 {\n\tid\n\tSubProduct2 {\n\tid\n\tname\n}\n}\n}",
             query.buildGraphQL(myProduct)
         )
     }
